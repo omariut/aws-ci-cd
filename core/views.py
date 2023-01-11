502af51d5a4ffc6a1c12e7c3a130c15a7a2aa332
omarcracktech <omar@cracktech.com.bd>
2023-01-11 17:10:24 +0600

    aws data added

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-   text = """<h1>Hi! this is our mind blowing home page</h1>"""
+   text = """<h1>Hello! this is our mind blowing home page</h1>"""
    return HttpResponse(text)
